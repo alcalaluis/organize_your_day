e14a62db93e6fed028b697eb3e91f52dffd2ec2e
alcalaluis <alcalaluis@rocketmail.com>
2020-10-30 22:00:07 -0500

    js script not reading/local storage fix

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ var descriptionInput = document.querySelector(".description");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 descriptionButton.addEventListener("click", function (event) {
     event.preventDefault();
 
     var textInput = descriptionInput.value;
-    localStorage.setItem("hour-9", textInput);
+    localStorage.setItem("hour-9", JSON.stringify(textInput));
 });
 
 
