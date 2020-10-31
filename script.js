ad3d941de027926a6255d326172af5c6e874ef7c
alcalaluis <alcalaluis@rocketmail.com>
2020-10-31 16:57:34 -0500

    local storage and moment.js addition

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
 
-var descriptionButton = document.querySelector(".btn");
-var descriptionInput = document.querySelector(".description");
 
 
-
-descriptionButton.addEventListener("click", function (event) {
+$("#button-9am").on("click", function (event) {
     event.preventDefault();
-
-    var textInput = descriptionInput.value;
-    localStorage.setItem("hour-9", JSON.stringify(textInput));
+    var textInput = $("#input-9am").val().trim();
+    localStorage.setItem("hour-9am", JSON.stringify(textInput));
 });
+
+$("#button-10am").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-10am").val().trim();
+    localStorage.setItem("hour-10am", JSON.stringify(textInput));
+});
+
+$("#button-11am").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-11am").val().trim();
+    localStorage.setItem("hour-11am", JSON.stringify(textInput));
+});
+
+$("#button-12pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-12pm").val().trim();
+    localStorage.setItem("hour-12pm", JSON.stringify(textInput));
+});
+
+$("#button-1pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-1pm").val().trim();
+    localStorage.setItem("hour-1pm", JSON.stringify(textInput));
+});
+
+$("#button-2pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-2pm").val().trim();
+    localStorage.setItem("hour-2pm", JSON.stringify(textInput));
+});
+
+$("#button-3pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-3pm").val().trim();
+    localStorage.setItem("hour-3pm", JSON.stringify(textInput));
+});
+
+$("#button-4pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-4pm").val().trim();
+    localStorage.setItem("hour-4pm", JSON.stringify(textInput));
+});
+
+$("#button-5pm").on("click", function (event) {
+    event.preventDefault();
+    var textInput = $("#input-5pm").val().trim();
+    localStorage.setItem("hour-5pm", JSON.stringify(textInput));
+});
+
+
+
+
 
 
 
